a4610faf9110b2e9200fbb98ce5dcb971b8cbc3b
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-04 18:38:43 -0700

    Step 5 completed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
+const mc = require('./controllers/messages_controller')
+
 const express = require('express')
 const app = express()
-
 
 //Configures the app to parse JSON from the body.
 app.use(express.json())
 
 app.listen(3001, () => console.log('unity on 3001!'))
+
+const baseURL = 'api/messages'
+app.post(baseURL, mc.create)
+app.get(baseURL, mc.read)
+app.put(`${baseURL}/:id`, mc.update)
+app.delete(`${baseURL}/:id`, mc.delete)
